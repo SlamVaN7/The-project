379cb13c9d181c62789a3a15ffbaf3f8a4c8725d
SlamVaN <sd22848002@gmail.com>
2025-09-11 16:46:22 +0200

    jumping

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -3,21 +3,29 @@ using UnityEngine.InputSystem;
 
 public class CharacterMovement : MonoBehaviour
 {
-    public float movementSpeed = 1f;
+    public float movementSpeed;
+    public float jumpHeight;
     void Update()
     {
-        if (InputSystem.GetDevice<Keyboard>().leftArrowKey.isPressed)
+        if (InputSystem.GetDevice<Keyboard>().aKey.isPressed)
         {
             gameObject.GetComponent<Rigidbody2D>().linearVelocity += Vector2.left
                                                                     * movementSpeed
                                                                     * Time.deltaTime;
         }
 
-        else if (InputSystem.GetDevice<Keyboard>().rightArrowKey.isPressed)
+        else if (InputSystem.GetDevice<Keyboard>().dKey.isPressed)
         {
             gameObject.GetComponent<Rigidbody2D>().linearVelocity += Vector2.right
                                                                     * movementSpeed
                                                                     * Time.deltaTime;
+        }
+
+        if (InputSystem.GetDevice<Keyboard>().spaceKey.wasPressedThisFrame)
+        {
+            gameObject.GetComponent<Rigidbody2D>().linearVelocity = Vector2.up
+                                                                    * jumpHeight;
+                                                                    
         }
     }
 }
